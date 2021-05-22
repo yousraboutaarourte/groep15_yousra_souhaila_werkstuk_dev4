d0f061b1527d03c79ce64eac217ff9b206f551be
SouhailaBenallal <44199863+SouhailaBenallal@users.noreply.github.com>
2021-05-22 12:51:31 +0200

    Add Cofees: Espresso, Cappuccino and Macchiato

--- JS/coffee/coffee.js
+++ JS/coffee/coffee.js
@@ -1,16 +1,39 @@
 "use strict";
-class Coffee {
-
-    getName() {
-        return this.name;
-    }
-
-    getDescription() {
-        return this.description;
-    }
-
-    getSize() {
-        return this.size;
+const Coffee = function() {
+    return {
+        Cappuccino: function(name, description, unitPrice, intensity, size) {
+            return {
+                name,
+                description,
+                unitPrice,
+                price: unitPrice,
+                intensity,
+                size,
+                toString: () => `A Cappuccino | Unit Price : ${unitPrice}€ | Extra : `
+            }
+        },
+        Espresso: function(name, description, unitPrice, intensity, size) {
+            return {
+                name,
+                description,
+                unitPrice,
+                price: unitPrice,
+                intensity,
+                size,
+                toString: () => `A Espresso | Unit Price : ${unitPrice}€ | Extra : `
+            }
+        },
+        Macchiato: function(name, description, unitPrice, intensity, size) {
+            return {
+                name,
+                description,
+                unitPrice,
+                price: unitPrice,
+                intensity,
+                size,
+                toString: () => `A Macchiato | Unit Price : ${unitPrice}€ | Extra : `
+            }
+        },
     }
 }
 
