2b6a03da40b9273be9bea755c6ce8e675a6da3ec
yousra boutaarourte <yousra.boutaarourte@student.ehb.be>
2021-05-22 21:23:01 +0200

    feat(class) added class Extra JavaScript

---
+++ JS/coffee/extra.js
@@ -0,0 +1,34 @@
+const Cream = (coffee) => {
+    return {
+        unitPrice: coffee.unitPrice,
+        price: 0.75 + coffee.price,
+        toString: () => coffee + " cream" //arrow functions 
+    }
+};
+
+const Sugar = (coffee) => {
+    return {
+        unitPrice: coffee.unitPrice,
+        price: 0.25 + coffee.price,
+        toString: function() { return coffee + " sugar" }
+    }
+}
+
+const Water = (coffee) => {
+    return {
+        unitPrice: coffee.unitPrice,
+        price: 0.10 + coffee.price,
+        toString: () => coffee + " water"
+    }
+}
+
+const Milk = (coffee) => {
+    return {
+        unitPrice: coffee.unitPrice,
+        price: 0.5 + coffee.price,
+        toString: () => coffee + " milk"
+    }
+}
+
+
+export { Cream, Sugar, Water, Milk };
