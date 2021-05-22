702d3f94971a3285309269dbd4b5cba88e09e101
yousra boutaarourte <yousra.boutaarourte@student.ehb.be>
2021-05-22 11:53:33 +0200

    add description coffee and cappuccino

--- Java/src/main/java/coffees/Cappuccino.java
+++ Java/src/main/java/coffees/Cappuccino.java
@@ -1,4 +1,7 @@
 package coffees;
 
 public class Cappuccino {
+    private String name;
+    private String description;
+    private double price;
 }
--- Java/src/main/java/coffees/Coffee.java
+++ Java/src/main/java/coffees/Coffee.java
@@ -1,4 +1,8 @@
 package coffees;
 
 public abstract class Coffee {
+    public abstract String getName();
+    public abstract String getDescription();
+    public abstract double getPrice();
+
 }
