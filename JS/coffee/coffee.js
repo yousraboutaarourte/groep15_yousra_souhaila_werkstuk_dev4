c21a2f9eb5a9290fcee7da2c97b3b5ef463edcd2
SouhailaBenallal <44199863+SouhailaBenallal@users.noreply.github.com>
2021-05-22 11:57:28 +0200

    Add description Javascript Coffees

--- JS/coffee/coffee.js
+++ JS/coffee/coffee.js
@@ -1,1 +1,17 @@
 "use strict";
+class Coffee {
+
+    getName() {
+        return this.name;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getSize() {
+        return this.size;
+    }
+}
+
+export { Coffee };
