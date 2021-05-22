13a904c70475e8ff0e213a24dcc1f747e8077f52
yousra boutaarourte <yousra.boutaarourte@student.ehb.be>
2021-05-22 21:33:43 +0200

    feat(class) added class Util JavaScript

--- JS/util/util.js
+++ JS/util/util.js
@@ -1,1 +1,16 @@
 "use strict";
+export const twoDigit = (number) => {
+    return parseFloat(number).toFixed(2);
+}
+
+export const colorForConsole = () => {
+    let $consoleLog = console.log;
+    console.log = function($message, $color) {
+        $consoleLog('%c' + $message, 'color:' + $color + ';font-weight:bold;');
+    }
+}
+
+export const displayInfoCoffee = (coffee) => {
+    return `${coffee} | Amount : ${twoDigit(coffee.price)} €`;
+
+}
