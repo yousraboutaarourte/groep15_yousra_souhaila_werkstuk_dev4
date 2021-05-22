74e4d2ef2257506d31bc8ec190b65f9727669245
SouhailaBenallal <44199863+SouhailaBenallal@users.noreply.github.com>
2021-05-22 12:59:32 +0200

    Add classes Cappucino, Espresso and Macchiato

--- Java/src/main/java/coffees/Cappuccino.java
+++ Java/src/main/java/coffees/Cappuccino.java
@@ -1,7 +1,37 @@
 package coffees;
 
-public class Cappuccino {
+public class Cappuccino extends Coffee {
     private String name;
     private String description;
     private double price;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
 }
--- Java/src/main/java/coffees/Espresso.java
+++ Java/src/main/java/coffees/Espresso.java
@@ -1,8 +1,36 @@
 package coffees;
 
-public class Espresso {
+public class Espresso extends Coffee {
     private String name;
     private String description;
     private double price;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
 
 }
